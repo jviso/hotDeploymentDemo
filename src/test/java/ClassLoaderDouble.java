6103eb700eb20589164ece913ef8c48f36f7b502
Jacob Visovatti <jvisovatti@Jacob-Visovatti.local>
2018-04-18 13:21:25 -0400

    remove commented out code

--- src/test/java/ClassLoaderDouble.java
+++ src/test/java/ClassLoaderDouble.java
@@ -2,11 +2,7 @@ public class ClassLoaderDouble extends ClassLoader {
 
     @Override
     public Class loadClass(String name, boolean test) throws ClassNotFoundException {
-//        if (Server.class.getName().equals(name)) {
-           return ServerSpy.class;
-//        } else {
-//            return Class.forName(name);
-//        }
-    }
+        return ServerSpy.class;
+   }
 
 }
